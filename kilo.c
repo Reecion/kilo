96c2b0e8f63cab02a6e04cc1459780ca616ea767
Reecion <61022054+Reecion@users.noreply.github.com>
2024-01-30 20:50:09 +0000

    _

--- kilo.c
+++ kilo.c
@@ -1,16 +1,199 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+struct editorConfig E;
+
+/*** terminal ***/
+
+// On Exit
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  perror(s);
+  exit(1);
+}
+
+// Restore terminal
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    die("tcsetattr");
+}
+// Enable raw mode for terminal
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  // Get current terminal into orig_termios
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+  
+  // Options for new terminal
+  struct termios raw = E.orig_termios;
+  // Disable Ctrl+M -> carriage return as \r\n
+  // Disable Ctrl+S and Ctrl+Q   --- sending and resuming input to terminal
+  // and Misc
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  // Disable ECHO 
+  // Canonical mode - reading whole line
+  // Ctrl+V (extension / waiting for next char input) also fixed Ctrl+0 on Mac
+  // Ctrl+C (terminate) and Ctrl+Z(Suspend)
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  // Disable translation \n into \r\n
+  raw.c_oflag &= ~(OPOST);
+  // Misc
+  raw.c_cflag |= (CS8);
+
+  // Timeout
+  // Min num of bytes before return
+  raw.c_cc[VMIN] = 0;
+  // Time after which to return (1 =  100ms)
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+// Reading the keys
+char editorReadKey() {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+
+  return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  // Get cursor position - check
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  // Read to buffer
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  // Printf expects last char to be this 
+  buf[i] = '\0';
+  // Error check
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  // Assign and check
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/*** append buffer ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  // Resize memory block that points to <pointer ab* to b> , new length <pointer ab* to len> + <new string length>
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  // Copy <destination> <source> <size>
+  memcpy(&new[ab->len], s, len);
+  // Update value
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
+/*** output ***/
+
+void editorDrawRows(struct abuf *ab) {
+  int y;
+  
+  for (y = 0; y < E.screenrows; y++) {
+    abAppend(ab, "~", 1);
+    if (y < E.screenrows - 1) {
+      abAppend(ab, "\r\n", 2);
+    }
+  }
+}
+
+void editorRefreshScreen() {
+  struct abuf ab = ABUF_INIT;
+  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+  abAppend(&ab, "\x1b[H", 3);
+  
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
+}
+
+/*** input ***/
+
+// Special characters / sequences processing
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('a'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
+/*** init ***/
+
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  initEditor();
+
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+
   return 0;
 }
